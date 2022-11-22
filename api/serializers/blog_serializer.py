b4f183aaa97498b7549bf275ca8d8d3be456f1a7
tunv <tunguyen060811@gmail.com>
2022-11-22 18:23:19 +0700

    feat: add blog id

--- api/serializers/blog_serializer.py
+++ api/serializers/blog_serializer.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 
 from ..models import Blog
 
 
 class BlogSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField('get_image_url')
+    blog_id = serializers.SerializerMethodField('get_blog_id')
 
     class Meta:
         model = Blog
         fields = [
+            'blog_id',
             'title',
             'description',
             'image_url',
@@ -19,6 +20,9 @@ class BlogSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         image_url = obj.image.url
         return request.build_absolute_uri(image_url)
+
+    def get_blog_id(self, obj):
+        return obj.id
 
     def create(self, validated_data):
         return super().create(validated_data)
