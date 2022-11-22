f2532a4d892e6d56f924a8bbfee8463db4660700
tunv <tunguyen060811@gmail.com>
2022-11-23 00:06:43 +0700

    feat: render all blog by call api

---
+++ frontend/static/js/home.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    render_blogs()
+
+})
+
+
+function render_blogs() {
+    $.ajax({
+        url: "http://127.0.0.1:8000/api/blogs",
+    }).done(function(blogs) {
+        for (let blog of blogs) {
+            let html = `
+                <article class="media content-section">
+                    <img class="rounded-circle article-img" src="">
+                    <div class="media-body">
+                        <div class="article-metadata">
+                            <a class="mr-2" href="">Tu Nguyen</a>
+                            <small class="text-muted">2022-01-01</small>
+                        </div>
+                        <h2><a class="article-title" href="">${blog.title}</a></h2>
+                        <p class="article-content">${blog.content}</p>
+                        <img class="blog-img" src="${blog.image_url}">
+                    </div>
+                </article>
+            `
+            $('#main').append(html)
+        }
+    });
+}
