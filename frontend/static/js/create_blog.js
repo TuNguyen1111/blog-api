72acb1f4b15f3338ca085cf34947bec51474b214
tunv <tunguyen060811@gmail.com>
2022-11-23 18:06:25 +0700

    feat: find textarea inputs

--- frontend/static/js/create_blog.js
+++ frontend/static/js/create_blog.js
@@ -7,7 +7,7 @@ function create_blog() {
     let endpoint = 'create/';
     let method = 'POST';
     let form_data = {};
-    for (let input of $('#create_form').find('input')) {
+    for (let input of $('#create_form').find('input, textarea')) {
         let input_name = $(input).attr('name');
         let input_val = $(input).val();
         form_data[input_name] = input_val;
