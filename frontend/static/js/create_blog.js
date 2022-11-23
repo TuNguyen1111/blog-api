f538863c45ab110f08234299dcef3aa57ee69e19
tunv <tunguyen060811@gmail.com>
2022-11-23 14:00:44 +0700

    feat: add create blog action

---
+++ frontend/static/js/create_blog.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#create_btn').click(create_blog);
+})
+
+
+function create_blog() {
+    let endpoint = 'create/';
+    let method = 'POST';
+    let form_data = {};
+    for (let input of $('#create_form').find('input')) {
+        let input_name = $(input).attr('name');
+        let input_val = $(input).val();
+        form_data[input_name] = input_val;
+    }
+    call_api(endpoint, method, form_data).done(function(res) {
+        $('#create_form')[0].reset();
+    })
+}
