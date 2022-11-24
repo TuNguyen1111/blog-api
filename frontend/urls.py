cc102ac3899bb6dfe9d63749950e4fea28f25fa5
tunv <tunguyen060811@gmail.com>
2022-11-24 23:54:26 +0700

    feat: remove login view

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create-blog', views.create_blog, name='create-blog'),
     path('detail-blog/<str:pk>', views.detail_blog, name='detail-blog'),
-    path('login/', views.login_view, name='login')
 ]
