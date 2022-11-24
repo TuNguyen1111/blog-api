3e15e05331fce31264d266b117e9709b16f8fa88
tunv <tunguyen060811@gmail.com>
2022-11-24 14:51:27 +0700

    feat: change to use fetch

--- frontend/static/js/common/api.js
+++ frontend/static/js/common/api.js
@@ -1,7 +1,20 @@
-function call_api(endpoint, method, form_data) {
-    return $.ajax({
-        url: `http://127.0.0.1:8000/api/${endpoint}`,
+async function call_api(endpoint, method, handle_success_response, form_data) {
+    let url = `http://127.0.0.1:8000/api/${endpoint}/`;
+    let options = {
         method: method,
-        data: form_data ? form_data : '',
-    })
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }
+
+    if (method == 'POST') {
+        options.body = form_data ? JSON.stringify(form_data) : ''
+    }
+
+    let response = await fetch(url, options);
+    if (response.status == 200) {
+        let data = await response.json()
+        handle_success_response(data);
+
+    }
 }
