1da5578fd28f3eaaf1cebde705d376e56fc8106e
tunv <tunguyen060811@gmail.com>
2022-11-24 15:15:16 +0700

    feat: save token

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -4,13 +4,29 @@ $(document).ready(function() {
 
 
 function login() {
-    let endpoint = 'token/';
+    let endpoint = 'token';
     let method = 'POST';
     let user_data = {};
     user_data['username'] = $('#username').val();
     user_data['password'] = $('#password').val();
 
-    call_api(endpoint, method, user_data).done(function(res) {
-        console.log(res)
-    })
+    call_api(endpoint, method, save_token, user_data);
+}
+
+
+function save_token(response) {
+    localStorage.setItem('refresh', response.refresh);
+    localStorage.setItem('access', response.access);
+    console.log(parse_Jwt(response.access))
+}
+
+
+function parse_Jwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
